17a3adf6884d3dbf803ede8bce9a922e0a33f7aa
Cortney Casper <ccasper5@liberty.edu>
2024-11-29 22:05:52 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys (replace with your own keys)
+TWITTER_API_KEY = "your_twitter_api_key"
+TWITTER_API_SECRET = "your_twitter_api_secret"
+ACCESS_TOKEN = "your_access_token"
+ACCESS_TOKEN_SECRET = "your_access_token_secret"
